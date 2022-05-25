dcc152d8d4cd6db524c15af0dc7eef9cfe88b264
git-mhasan <mahadi2020dev@gmail.com>
2022-05-25 23:13:40 +0600

    jwt implemented on server side.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -11,6 +12,22 @@ app.use(cors());
 app.use(express.json());
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+
 // MongoDb connecion string
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1moqz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -20,6 +37,7 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db('horizonDb').collection("products");
+        const userCollection = client.db('horizonDb').collection("users");
 
         // Get all products
         app.get('/products', async (req, res) => {
@@ -40,9 +58,21 @@ async function run() {
                 const product = await productCollection.findOne(query);
                 res.send(product);
             }
-
         });
 
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            console.log(email);
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+        });
     }
     catch (error) {
         console.error(error);
